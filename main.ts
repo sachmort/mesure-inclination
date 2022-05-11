d3bc23cd51a2000e983d7e91e0af29384267c0af
sachmort <103418654+sachmort@users.noreply.github.com>
2022-05-11 11:15:55 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,27 @@
+basic.forever(function () {
+    if (input.acceleration(Dimension.X) < -100) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (input.acceleration(Dimension.X) < 100) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    }
+})
